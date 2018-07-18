bc64cda9c0ffbf2cb07b90168723437706a40ede
kylehovey <khoveyc@gmail.com>
2018-07-18 16:31:52 -0600

    Added maybe monad definition.

---
+++ maybe.js
@@ -0,0 +1,15 @@
+const MType = Symbol("Maybe");
+const isMaybe = x => (x instanceof Object && x.type === MType);
+const isNone = x => x === null || x === undefined;
+
+const Just = (x, last) => ({
+  value : () => x,
+  or : alternative => Maybe(isNone(x) ? alternative : x, x),
+  then : f => Maybe(isNone(x) ? x : f(x), x),
+  catch : f => Maybe(isNone(x) ? f(last) : x, x),
+  type : MType
+});
+
+const Maybe = (x, last = null) => isMaybe(x) ? x : Just(x, last)
+
+module.exports = Maybe;
